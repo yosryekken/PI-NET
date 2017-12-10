9203a34d6a7a36036fde710efe6defba1a70f544
Yekken Yosr <yosr.yekken@esprit.tn>
2017-12-10 22:44:18 +0000

    Update users.cs

--- Domain/Entites/users.cs
+++ Domain/Entites/users.cs
@@ -9,6 +9,22 @@ namespace Domain.Entites
     [Table("gestionsinistre.users")]
     public partial class users
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public users()
+        {
+            demande = new HashSet<demande>();
+            messages = new HashSet<messages>();
+            participate = new HashSet<participate>();
+            police = new HashSet<police>();
+            reclamation = new HashSet<reclamation>();
+            reponse = new HashSet<reponse>();
+            sinister = new HashSet<sinister>();
+            souscategory = new HashSet<souscategory>();
+            topic = new HashSet<topic>();
+            vehicle = new HashSet<vehicle>();
+            evenement = new HashSet<evenement>();
+        }
+
         public int id { get; set; }
 
         public DateTime? BirthdayDate { get; set; }
@@ -33,5 +49,38 @@ namespace Domain.Entites
 
         [StringLength(255)]
         public string role { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<demande> demande { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<messages> messages { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<participate> participate { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<police> police { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<reclamation> reclamation { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<reponse> reponse { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<sinister> sinister { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<souscategory> souscategory { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<topic> topic { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<vehicle> vehicle { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<evenement> evenement { get; set; }
     }
 }
