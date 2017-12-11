b1cf3b72ede2a9b100672895b85277092408981e
Yekken Yosr <yosr.yekken@esprit.tn>
2017-12-11 23:33:39 +0000

    Update Service.cs

--- Pattern/Service.cs
+++ Pattern/Service.cs
@@ -1,87 +1,105 @@
-﻿
+
+
+
+using Data.Infrastructure;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 
-
-
-namespace Service.Pattern
+namespace Service.Patern
 {
-    public class Service<TEntity> : IService<TEntity> 
-        where TEntity : class
+    public abstract class Service<TEntity> : IService<TEntity> where TEntity : class
     {
         #region Private Fields
+        // private readonly IRepositoryBaseAsync<TEntity> _repository;
         IUnitOfWork utwk;
         #endregion Private Fields
 
         #region Constructor
         protected Service(IUnitOfWork utwk)
-        {
+        {                                                               
             this.utwk = utwk;
         }
         #endregion Constructor
 
 
-        public void Dispose()
-        {
-            utwk.Dispose();
-        }
+
         public virtual void Add(TEntity entity)
         {
             ////_repository.Add(entity);
-            utwk.getRepository<TEntity>().Add(entity);
+            utwk.GetRepository<TEntity>().Add(entity);
+            
         }
 
         public virtual void Update(TEntity entity)
         {
             //_repository.Update(entity);
-            utwk.getRepository<TEntity>().Update(entity);
+            utwk.GetRepository<TEntity>().Update(entity);
         }
 
         public virtual void Delete(TEntity entity)
         {
             //   _repository.Delete(entity);
-            utwk.getRepository<TEntity>().Delete(entity);
+            utwk.GetRepository<TEntity>().Delete(entity);
         }
 
         public virtual void Delete(Expression<Func<TEntity, bool>> where)
         {
             // _repository.Delete(where);
-            utwk.getRepository<TEntity>().Delete(where);
+            utwk.GetRepository<TEntity>().Delete(where);
         }
 
         public virtual TEntity GetById(long id)
         {
             //  return _repository.GetById(id);
-            return utwk.getRepository<TEntity>().GetById(id);
+           return utwk.GetRepository<TEntity>().GetById(id);
         }
 
         public virtual TEntity GetById(string id)
         {
             //return _repository.GetById(id);
-            return utwk.getRepository<TEntity>().GetById(id);
+            return utwk.GetRepository<TEntity>().GetById(id);
         }
 
         public virtual IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> filter = null, Expression<Func<TEntity, bool>> orderBy = null)
         {
             //  return _repository.GetAll();
-            return utwk.getRepository<TEntity>().GetMany(filter,orderBy);
-        }
-        public virtual IEnumerable<TEntity> GetAll()
-        {
-              return utwk.getRepository<TEntity>().getAll();
+            return utwk.GetRepository<TEntity>().GetMany(filter);
         }
 
         public virtual TEntity Get(Expression<Func<TEntity, bool>> where)
         {
             //return _repository.Get(where);
-            return utwk.getRepository<TEntity>().Get(where);
+            return utwk.GetRepository<TEntity>().Get(where);
+        }
+        /*
+        public virtual async Task<int> CountAsync()
+        {
+            //return await _repository.CountAsync();
+            return await utwk.GetRepository<TEntity>().CountAsync();
         }
 
+        public virtual async Task<List<TEntity>> GetAllAsync()
+        {
+            //return await _repository.GetAllAsync();
+            return await utwk.GetRepository<TEntity>().GetAllAsync();
+        }
 
+        public virtual async Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> match)
+        {
+            //return await _repository.FindAsync(match);
+            return await utwk.GetRepository<TEntity>().FindAsync(match);
+        }
+
+        public virtual async Task<List<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> match)
+        {
+            // return await _repository.FindAllAsync(match);
+            return await utwk.GetRepository<TEntity>().FindAllAsync(match);
+        }
+
+       */
+       
         public void Commit()
         {
             try
@@ -91,11 +109,11 @@ namespace Service.Pattern
             catch (Exception ex)
             {
 
-                throw;
+            
             }
         }
 
-        public void CommitAsync()
+       /* public void CommitAsync()
         {
             try
             {
@@ -106,26 +124,17 @@ namespace Service.Pattern
 
                 throw;
             }
+        }*/
+
+        public void Dispose()
+        {
+            utwk.Dispose();
         }
 
-        public Task<int> CountAsync()
+        public IEnumerable<TEntity> GetAll()
         {
-            throw new NotImplementedException();
-        }
 
-        public Task<List<TEntity>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> match)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> match)
-        {
-            throw new NotImplementedException();
+            return utwk.GetRepository<TEntity>().GetAll();
         }
     }
 }
