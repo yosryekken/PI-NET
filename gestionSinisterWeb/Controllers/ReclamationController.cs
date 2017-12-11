8f314c7ac7d2558df94767a388de0e8c225a7f2b
yosryekken <yosr.yekken@esprit.tn>
2017-12-11 23:47:57 +0000

    Controlleur recalamtion + Controlleur user

---
+++ gestionSinisterWeb/Controllers/ReclamationController.cs
@@ -0,0 +1,42 @@
+﻿using gestionSinisterWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Entites;
+using Service;
+
+namespace gestionSinisterWeb.Controllers
+{
+    public class ReclamationController : Controller
+    {
+        ServiceUser SU = null;
+        ServiceReclamation rs = null;
+
+        [HttpPost]
+        public ActionResult Reclamation(ViewReclamationModel rvm)
+        {
+            SU = new ServiceUser();
+            rs = new ServiceReclamation();
+            reclamation r = new reclamation();
+            r.description = rvm.description;
+            r.sujet = rvm.sujet;
+            
+            rs.Add(r);
+            rs.Commit();
+
+            SU.email(rvm.Email, rvm.Password, rvm.sujet, rvm.description);
+            ViewBag.Message = "Your Profile page.";
+
+            return View();
+        }
+
+
+        public ActionResult Reclamation()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ gestionSinisterWeb/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using gestionSinisterWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gestionSinisterWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(ViewUserModel uvm)
+        {
+
+
+            HttpClient Client = new HttpClient();
+
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Client.DefaultRequestHeaders.Accept.Clear();
+            HttpResponseMessage response;
+            String cnx = "http://localhost:18080/JEEPI-web/rest/users/" + uvm.login + "/" + uvm.password;
+            if (uvm.login != null && uvm.password != null)
+
+            {
+                response = Client.GetAsync(cnx).Result;
+            }
+            else
+            {
+                response = null;
+            }
+
+            if (response.Content.Headers.ContentLength != 0)
+            {
+                try { 
+                uvm = response.Content.ReadAsAsync<ViewUserModel>().Result;
+
+                ViewBag.result = response.Content.ReadAsAsync<ViewUserModel>().Result;
+                }catch (Exception e)
+                {
+                    
+                }
+                // IEnumerable<Users> u = response.Content.ReadAsAsync<IEnumerable<Users>>().Result;
+
+                return
+                    RedirectToAction("Home","Home", uvm);
+            }
+            else
+            {
+                //n3adi fl view lerreur
+                return View();
+            }
+
+        }
+
+
+    }
+}
