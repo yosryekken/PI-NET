708a291e078d43b7cf60863a4ce706bad4a3971e
yosryekken <yosr.yekken@esprit.tn>
2017-12-11 23:44:19 +0000

    Service Reclamation + Service User

---
+++ Service/ServiceReclamation.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Infrastructure;
+using Domain.Entites;
+using Service.Patern;
+
+namespace Service
+{
+    public class ServiceReclamation : Service<reclamation>
+    {
+        private static IDatabaseFactory dbf = new DatabaseFactory();
+        private static IUnitOfWork utwk = new UnitOfWork(dbf);
+
+    
+
+        public ServiceReclamation() : base(utwk)
+        {
+        }
+    }
+}
---
+++ Service/ServiceUser.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Infrastructure;
+using System.Net.Mail;
+using System.Net;
+using Service.Patern;
+
+namespace Service
+{
+    public class ServiceUser : Service<users>
+    {
+        private static IDatabaseFactory dbf = new DatabaseFactory();
+        private static IUnitOfWork utwk = new UnitOfWork(dbf);
+
+        public ServiceUser() : base(utwk)
+        {
+        }
+
+        public void email(String email, String pass, String subject, String body)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(email, pass);
+                MailMessage msg = new MailMessage();
+                msg.To.Add("yosr.yekken@esprit.tn");
+                msg.From = new MailAddress(email);
+                msg.Subject = subject;
+                msg.Body = body;
+                client.Send(msg);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
