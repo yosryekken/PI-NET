1df579a3214a6d4f29eab1411572092e558c75d0
marwabenabdeljawed <marwa.benabdeljawed@esprit.tn>
2017-12-11 18:11:38 +0100

    Update EventModels.cs

--- gestionSinisterWeb/Models/EventModels.cs
+++ gestionSinisterWeb/Models/EventModels.cs
@@ -1,33 +1,42 @@
-﻿using System;
+using pi.domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace gestionSinisterWeb.Models
+namespace pi.web.Models
 {
-    public class EventModels
+    public class EvenementModel
     {
         [Key]
-        public int id { get; set; }
+        public int EvenementId { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Start_Date")]
         [DataType(DataType.Date)]
-        public DateTime? dateDebut { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Start_Date { get; set; }
 
+
+
+        [Display(Name = "Finish_Date")]
         [DataType(DataType.Date)]
-        public DateTime? dateFin { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Finish_Date { get; set; }
+        [DataType(DataType.ImageUrl)]
+        public String Image { get; set; }
+        public String Description { get; set; }
 
-        [DataType(DataType.MultilineText)]
-        public string description { get; set; }
+        public String Localisation { get; set; }
 
-        public TimeSpan? heure { get; set; }
+        public int UserId { get; set; }
 
-        [StringLength(255)]
-        public string localisation { get; set; }
+        public int AgenceId { get; set; } = 1;
 
-        public int? nbrmaxpart { get; set; }
-
-        [StringLength(255)]
-        public string titre { get; set; }
+        public virtual ICollection<Participant> Participants { get; set; }
     }
 }
