c4d7cf11b57cfc286be8da08c76536e188a494de
yosryekken <yosr.yekken@esprit.tn>
2017-12-11 23:49:22 +0000

    Model recalamtion + Model user

---
+++ gestionSinisterWeb/Models/ViewReclamationModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace gestionSinisterWeb.Models
+{
+    public class ViewReclamationModel
+    {
+        public int id { get; set; }
+
+        [StringLength(255)]
+        public string description { get; set; }
+
+        [StringLength(255)]
+        public string titre { get; set; }
+
+        public int? idInsured { get; set; }
+
+        [StringLength(255)]
+        public string sujet { get; set; }
+
+        [StringLength(255)]
+        public string typeRec { get; set; }
+
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [StringLength(255)]
+        public string Password { get; set; }
+    }
+}
---
+++ gestionSinisterWeb/Models/ViewUserModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace gestionSinisterWeb.Models
+{
+    public class ViewUserModel
+    {
+
+
+        public int id { get; set; }
+
+        public DateTime? BirthdayDate { get; set; }
+
+        [StringLength(255)]
+        public string adress { get; set; }
+
+        [StringLength(255)]
+        public string email { get; set; }
+
+        [StringLength(255)]
+        public string login { get; set; }
+
+        [StringLength(255)]
+        public string name { get; set; }
+
+        [StringLength(255)]
+        public string password { get; set; }
+
+        [StringLength(255)]
+        public string phone { get; set; }
+
+        [StringLength(255)]
+        public string role { get; set; }
+    }
+}
